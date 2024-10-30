3711d6e773f60cc567c77a0798c9631502fc8d9f
AsherMathewsKoshy <ashermathewskoshy@gmail.com>
2024-10-30 10:21:10 +0530

    Added search functionality for destinations

--- destinations/views.py
+++ destinations/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
 from rest_framework import viewsets
 from .models import Destination
 from .serializers import DestinationSerializer
